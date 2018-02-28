fd134694ac0933402698bc6b1ed0e9e703f679b2
NadezhdaWebDeveloper <nadezhda2991@gmail.com>
2018-02-28 18:36:08 +0200

    artwork - single page

--- store/index.js
+++ store/index.js
@@ -88,43 +88,48 @@ export default () => {
         
         let artwork = {};
         data.map(item => {
-          artwork = {
-            id: item.id !== undefined ? item.id : '',
-            artist: item.acf.artwork_artist_label !== undefined ? item.acf.artwork_artist_label : '',
-            year: item.acf.artwork_year,
-            title: item.title.rendered,
-            slug: item.slug
-          };
-          if (item._embedded["wp:featuredmedia"] !== undefined) {
-            item._embedded["wp:featuredmedia"].map(el => artwork.image_url = el.source_url);				
-          }
-          if (item._embedded["wp:term"] !== undefined) {
-            item._embedded["wp:term"].map(el => {
-              el.map( i => artwork.category = i.name );
-            });
-          }
-          artwork.link = getUrl(item.link).pathname;          
-          artwork.gallery = [];
-          item.acf.artwork_gallery.map(item => {
-            artwork.gallery.push({
-              thumb: {
-                imageUrl: item.sizes.thumbnail,
-                imageWidth: item.sizes['thumbnail-width'],
-                imageHeight: item.sizes['thumbnail-height']
-              },
-              medium: {
-                imageUrl: item.sizes.medium,
-                imageWidth: item.sizes['medium-width'],
-                imageHeight: item.sizes['medium-height']
-              }
-            });
-          });
+          artwork = this.dispatch('artworkInit', item);
+          console.log('getArtwork Action', artwork);
+          
         });
         commit('setArtwork', artwork);
       },
 
       artworkInit(item) {
-
+        console.log('artworkInit item:', item);
+        
+        let artwork = {
+          id: item.id !== undefined ? item.id : '',
+          artist: item.acf.artwork_artist_label !== undefined ? item.acf.artwork_artist_label : '',
+          year: item.acf.artwork_year,
+          title: item.title.rendered,
+          slug: item.slug
+        };
+        if (item._embedded["wp:featuredmedia"] !== undefined) {
+          item._embedded["wp:featuredmedia"].map(el => artwork.image_url = el.source_url);				
+        }
+        if (item._embedded["wp:term"] !== undefined) {
+          item._embedded["wp:term"].map(el => {
+            el.map( i => artwork.category = i.name );
+          });
+        }
+        artwork.link = getUrl(item.link).pathname;          
+        artwork.gallery = [];
+        item.acf.artwork_gallery.map(item => {
+          artwork.gallery.push({
+            thumb: {
+              imageUrl: item.sizes.thumbnail,
+              imageWidth: item.sizes['thumbnail-width'],
+              imageHeight: item.sizes['thumbnail-height']
+            },
+            medium: {
+              imageUrl: item.sizes.medium,
+              imageWidth: item.sizes['medium-width'],
+              imageHeight: item.sizes['medium-height']
+            }
+          });
+        });
+        return artwork;
       }
     }
   })
