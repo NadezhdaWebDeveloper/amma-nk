0756123b573e82d965226fdbc099be4a19ae27ff
NadezhdaWebDeveloper <nadezhda2991@gmail.com>
2018-03-01 10:30:34 +0200

    artwork - single page

--- store/index.js
+++ store/index.js
@@ -42,38 +42,13 @@ export default () => {
         const { headers, data } = await api.getArtworks(queryString);        
         let finalData = [];        
         data.map(item => {
-          let artwork = {
-            id: item.id !== undefined ? item.id : '',
-            artist: item.acf.artwork_artist_label !== undefined ? item.acf.artwork_artist_label : '',
-            year: item.acf.artwork_year,
-            title: item.title.rendered,
-            slug: item.slug
-          };
-          if (item._embedded["wp:featuredmedia"] !== undefined) {
-            item._embedded["wp:featuredmedia"].map(el => artwork.image_url = el.source_url);				
-          }
-          if (item._embedded["wp:term"] !== undefined) {
-            item._embedded["wp:term"].map(el => {
-              el.map( i => artwork.category = i.name );
-            });
-          }
-          artwork.link = getUrl(item.link).pathname;          
-          artwork.gallery = [];
-          item.acf.artwork_gallery.map(item => {
-            artwork.gallery.push({
-              thumb: {
-                imageUrl: item.sizes.thumbnail,
-                imageWidth: item.sizes['thumbnail-width'],
-                imageHeight: item.sizes['thumbnail-height']
-              },
-              medium: {
-                imageUrl: item.sizes.medium,
-                imageWidth: item.sizes['medium-width'],
-                imageHeight: item.sizes['medium-height']
-              }
-            });
+          this.dispatch('artworkInit', item)
+          .then(res => {
+            finalData.push(res);
+          })
+          .catch(err => {
+            throw new Error(err);
           });
-          finalData.push(artwork);
         });
         commit('setArtworks', {
           data: finalData,
@@ -84,20 +59,18 @@ export default () => {
       async getArtwork({ commit }, route) {        
         let query = route.params.id != undefined ? `&filter[name]=${route.params.id}` : '';
         const { data } = await api.getArtwork(query);
-        console.log(data);
-        
-        let artwork = {};
         data.map(item => {
-          artwork = this.dispatch('artworkInit', item);
-          console.log('getArtwork Action', artwork);
-          
+          this.dispatch('artworkInit', item)
+            .then(res => {
+              commit('setArtwork', res);
+            })
+            .catch(err => {
+              throw new Error(err);
+            });
         });
-        commit('setArtwork', artwork);
       },
 
-      artworkInit(item) {
-        console.log('artworkInit item:', item);
-        
+      artworkInit(context, item) {        
         let artwork = {
           id: item.id !== undefined ? item.id : '',
           artist: item.acf.artwork_artist_label !== undefined ? item.acf.artwork_artist_label : '',
